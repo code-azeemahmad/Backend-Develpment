6ff8d7c5fd67fa1a15fe0bc40214bfe02a88f9a1
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-18 16:36:47 +0500

    Add updateAccountDetails controller

--- refresh_access_token_end_point/src/controllers/user.controller.js
+++ refresh_access_token_end_point/src/controllers/user.controller.js
@@ -272,6 +272,34 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
 });
 
+// make separate controllers for updating file data
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullname, email } = req.body;
+
+  if (!fullname && !email) {
+    throw new ApiError(400, "All fields are mandatory!");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req?.user?._id,
+    {
+      $set: {
+        // updates only specified fields
+        fullname,
+        email,
+      },
+    },
+    {new: true}   // return updated document
+  ).select("-password -refreshToken");
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user, "Account details updated successfully!"));
+});
+
+
+
 
 export {
   registerUser,
@@ -280,4 +308,5 @@ export {
   refreshAccessToken,
   changeCurrentPassword,
   getCurrentUser,
+  updateAccountDetails,
 };
