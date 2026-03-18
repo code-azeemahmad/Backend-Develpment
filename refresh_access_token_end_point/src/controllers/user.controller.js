c855181dd66f6614e819f5a114cf8ea75171198c
code-azeemahmad <azeemahmadd9@gmail.com>
2026-03-18 16:35:58 +0500

    Add getCurrentUser controller

--- refresh_access_token_end_point/src/controllers/user.controller.js
+++ refresh_access_token_end_point/src/controllers/user.controller.js
@@ -266,6 +266,11 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully!"));
 });
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
+});
 
 
 export {
@@ -274,4 +279,5 @@ export {
   logoutUser,
   refreshAccessToken,
   changeCurrentPassword,
+  getCurrentUser,
 };
